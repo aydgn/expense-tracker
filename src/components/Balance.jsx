90379c9f1e5e6ea5fecb485e97afecf33c9ca607
Aydoğan <aydogan.gokberk@gmail.com>
2021-08-15 20:48:11 +0300

    transaction completed

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+function moneyFormatter(num) {
+  let p = num.toFixed(2).split(".");
+  return (
+    "$ " +
+    p[0]
+      .split("")
+      .reverse()
+      .reduce(function (acc, num, i) {
+        return num === "-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
+      }, "") +
+    "." +
+    p[1]
+  );
+}
 
 export const Balance = () => {
+  // Get the current balance from the context
+  const { transactions } = useContext(GlobalContext);
+
+  // Return the balance
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  // Get the sum of all the transaction amounts
+  const total = amounts.reduce((acc, item) => (acc += item), 0);
+
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>{moneyFormatter(total)}</h1>
     </>
   );
 };
